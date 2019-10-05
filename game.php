a9d62cb4be73a27072fac87a60831263e4b62690
1-8192 <allegranzia@gmail.com>
2019-10-05 18:00:06 -0400

    game logic is working and displaying properly

--- game.php
+++ game.php
@@ -11,7 +11,43 @@
     }
 
     // game logic 
-    $player = isset($_POST["player"]) ? $_POST["player"] : null;
+    $player = isset($_POST["player"]) ? $_POST["player"] : 4;
+    $rps_array = ["Rock", "Paper", "Scissors"];
+    $computer = "";
+
+    function check($player) {
+        global $computer;
+        $computer = rand(0,2);
+        if ($player == 0) {
+            if ($computer == 0) {
+                return "Tie";
+            } else if ($computer == 1) {
+                return "You Lose";
+            } else if ($computer == 2) {
+                return "You Win";
+            }
+        } else if ($player == 1) {
+            if ($computer == 0) {
+                return "You Win";
+            } else if ($computer == 1) {
+                return "Tie";
+            } else if ($computer == 2) {
+                return "You Lose";
+            }
+        } else if ($player == 2) {
+            if ($computer == 0) {
+                return "You Lose";
+            } else if ($computer == 1) {
+                return "You Win";
+            } else if ($computer == 2) {
+                return "Tie";
+            }
+        } else {
+            return false;
+        }
+    }
+
+    $result = check($player);
 
 ?>
 
@@ -23,20 +59,28 @@
     </head>
     <body>
         <h1>Rock Paper Scissors</h1>
-        <p>Welcome: <?= $_GET["name"] ?> </p>
+        <!-- checking to make sure HTML injection doesn't happen -->
+        <p>Welcome: <?= htmlentities($_GET['name']) ?> 
+        </p>
         <form method="POST">
         <select name="player">
-        <option value="0">Select</option>
-        <option value="1">Rock</option>
-        <option value="2">Paper</option>
-        <option value="3">Scissors</option>
-        <option value="4">Test</option>
+        <option value="4">Select</option>
+        <option value="0">Rock</option>
+        <option value="1">Paper</option>
+        <option value="2">Scissors</option>
+        <option value="3">Test</option>
         </select>
         <input type="submit" value="Play">
         <input type="submit" name="cancel" value="Lougout">
         </form>
         <p>
-            Please select a strategy and press Play.
+            <?php
+            if ($player == 4) {
+                print "Please select a strategy and press Play.";
+            } else {
+                print "Your play=$rps_array[$player] Computer's play=$rps_array[$computer] Result=$result \n";
+            }
+            ?>
         </p>
     </body>
 </html>
