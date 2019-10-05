9c6aaa942415a190a6dc46383386325443190181
1-8192 <allegranzia@gmail.com>
2019-10-05 15:29:50 -0400

    created rest of the files and the welcome page

--- index.php
+++ index.php
@@ -5,5 +5,11 @@
         <?php require_once "bootstrap_styling.php" ?>
     </head>
     <body>
+        <h1>Welcome to Rock Paper Scissors</h1>
+        <a href="login.php">Please Log In</a>
+        <p>
+            Atempt to go to <a href="game.php">game.php</a> without loggin in - it should fail with an error message.
+        </p>
+        <a href="http://www.wa4e.com/code/rps.zip">Specification for this application</a>
     </body>
 </html>
---
+++ login.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Alessandro Allegranzi's Rock Paper Scissors Game</title>
+        <?php require_once "bootstrap_styling.php" ?>
+    </head>
+    <body>
+        <h1>TEST</h1>
+    </body>
+</html>
