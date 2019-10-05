7e6f0ddf800bc494ac2bc64765b69369d51d9146
1-8192 <allegranzia@gmail.com>
2019-10-05 17:29:06 -0400

    redirecting succesfully on logout

--- game.php
+++ game.php
@@ -1,3 +1,10 @@
+<?php
+    if ( isset($_POST['cancel'])) {
+        header('Location: index.php');
+        return;
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -5,6 +12,21 @@
         <?php require_once "bootstrap_styling.php" ?>
     </head>
     <body>
-        <h1>TEST</h1>
+        <h1>Rock Paper Scissors</h1>
+        <p>Welcome: name </p>
+        <form method="POST">
+        <select name="">
+        <option value="0">Select</option>
+        <option value="1">Rock</option>
+        <option value="2">Paper</option>
+        <option value="3">Scissors</option>
+        <option value="4">Test</option>
+        </select>
+        <input type="submit" value="Play">
+        <input type="submit" name="cancel" value="Lougout">
+        </form>
+        <p>
+            Please select a strategy and press Play.
+        </p>
     </body>
 </html>
